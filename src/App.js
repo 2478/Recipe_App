ace8197373be90edd5e21b5ad0b1cd4cd954283b
2478 <minarva.das92@gmail.com>
2021-07-09 12:13:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import Recipe from "./Recipe";
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID ="108faac9";
+  const APP_KEY ="02b4b428202e46ab065c50c834d2d0e6";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken');
+  //const exampleReq =`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  //const [counter, setCounter] = useState(0);
+
+    useEffect( () => {
+      getRecipes();
+      
+    }, [query]);
+
+    const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+      setRecipes(data.hits);
+      console.log(data.hits);
+    };
+
+    const updateSearch = e => {
+      setSearch(e.target.value);
+      //console.log(search);
+    };
+
+    const getSearch = e => {
+      e.preventDefault();
+      setQuery(search);
+      setSearch('');
+    };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+      {recipes.map(recipe => (
+        <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+          />
+      ))}
+    </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/r.js
@@ -0,0 +1,13 @@
+import React from 'react';  
+import {component} from 'react';  
+class Second extends Component 
+{ 
+    render() 
+    { 
+        return (
+            <div className="App"> 
+        <h1>Class Based Component</h1></div> 
+        );
+    }
+}  
+        export default Second;
